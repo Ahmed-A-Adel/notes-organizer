cd62fd76268e2c62f5f65980e730c9d8f1d171df
Ahmed Adel <AhmedAdel.PersonalEmail@gmail.com>
2025-05-13 07:33:57 +0300

    refactor index.js stateMethods obj an removed model.js comments

--- scripts/index.js
+++ scripts/index.js
@@ -17,35 +17,24 @@ import view from "./views/View.js";
 // Take the common methods and props from all views to a single parent view.
 // Shared fileds name yet specefic to one class would give us a true power of using inherited methods from parent without defining them
 const stateMethods = {
-  getMode: getMode,
-  toggleMode: toggleMode,
-  editPrevNotes: editPrevNotes,
-  deletePrevNote: deletePrevNote,
-  updateNotes: updateNotes,
-  getNotes: getNotes,
+  getMode,
+  toggleMode,
+  editPrevNotes,
+  deletePrevNote,
+  updateNotes,
+  getNotes,
 };
-// const stateMethods = {
-//   getMode: state.getMode.bind(state),
-//   toggleMode: state.toggleMode.bind(state),
-//   editPrevNotes: state.editPrevNotes.bind(state),
-//   deletePrevNote: state.deletePrevNote.bind(state),
-//   updateNotes: state.updateNotes.bind(state),
-//   getNotes: state.getNotes.bind(state),
-// };
 //______________________________________________________________;
 function loadNotesHandler() {
   loadNotes(sideNoteView.renderPrevNotes.bind(sideNoteView));
-  // state.loadNotes(sideNoteView.renderPrevNotes.bind(sideNoteView));
 }
 //______________________________________________________________;
 // Filter view methods to do only one specific action and the left actions like updateding the state and side effects will be handled here in the contoller
 // the methods that doesn't need callBAck handler should be called in the view constructor
 function controlSaveNote(props, edit = false) {
   let newNotes = edit ? saveNoteOnEdit(props) : addNote(props);
-  // let newNotes = edit ? state.saveNoteOnEdit(props) : state.addNote(props);
   sideNoteView.renderPrevNotes(newNotes);
   updateNotes(newNotes);
-  // state.updateNotes(newNotes);
 }
 //_____________________________________________________`_________;
 
--- scripts/modle.js
+++ scripts/modle.js
@@ -1,5 +1,4 @@
 import { addToStorage, getFromStorage } from "./helpers.js";
-// transform Class State into object and see the change.
 const state = {
   darkmode: false,
   editMode: false,
@@ -114,124 +113,6 @@ export function getNotes() {
   const notes = state.notes;
   return notes;
 }
-console.log(state);
-// class State {
-//   darkmode = false;
-//   editMode = false;
-//   listMode = false;
-//   tagMode = false;
-//   pointMode = false;
-//   fullViewMode = false;
-//   sideNotesMode = true;
-//   listItems = [];
-//   date = "";
-//   time = "";
-//   constructor() {
-//     this.notes = [
-//       {
-//         id: 1,
-//         title: "title 1",
-//         content: "content",
-//         complate: true,
-//         tags: [],
-//         edit: false,
-//         order: 1,
-//       },
-//       {
-//         id: 2,
-//         title: "title 2",
-//         content: "content",
-//         complate: true,
-//         tags: [],
-//         edit: false,
-//         order: 2,
-//       },
-//     ];
-//   }
-
-//   addNote(props) {
-//     // Custom made ID
-//     const noteId = new Uint32Array(1);
-//     crypto.getRandomValues(noteId);
-//     // Custom made ID
-
-//     const newNote = {
-//       id: noteId[0],
-//       date: this.date,
-//       time: this.time,
-//       complate: true,
-//       edit: false,
-//       order: this.notes.length ?? +1,
-//       ...props,
-//     };
-//     const newNotes = [newNote, ...this.notes];
-
-//     addToStorage("notes", newNotes);
-//     this.notes = newNotes;
-//     return newNotes;
-//   }
-//   saveNoteOnEdit(props) {
-//     const note = this.notes.filter((note) => note.edit)[0];
-//     const slicedNotes = this.notes.filter((note) => !note.edit);
-//     const newNotes = [
-//       {
-//         ...note,
-//         ...props,
-//         edit: false,
-//       },
-//       ...slicedNotes,
-//     ];
-
-//     addToStorage("notes", newNotes);
-//     this.notes = newNotes;
-//     this.toggleMode("editMode");
-//     return newNotes;
-//   }
-//   editPrevNotes(note, id) {
-//     // Save editNote (animation) on reload !!
-//     const notes = [
-//       ...this.notes
-//         .filter((note) => note.id != id)
-//         .map((note) => {
-//           return { ...note, edit: false };
-//         }),
-//       { ...note, edit: !note.edit },
-//     ];
-//     this.notes = notes;
-//     return notes;
-//   }
-//   deletePrevNote(id) {
-//     const notes = [...this.notes.filter((note) => note.id != id)];
-//     this.notes = notes;
-//     addToStorage("notes", notes);
-//     return notes;
-//   }
-
-//   toggleMode(name, value = "") {
-//     const oldValue = this[name];
-//     const inValue = typeof value == "boolean" ? value : !oldValue;
-//     this[name] = inValue;
-//   }
-//   getMode(name) {
-//     const value = this[name];
-//     console.log("getMode", name, ":", value);
-//     return value;
-//   }
-//   loadNotes(renderPrevNotes) {
-//     const notes = getFromStorage("notes") || this.notes;
-//     renderPrevNotes(notes);
-//     this.notes = notes;
-//   }
-//   updateNotes(notes) {
-//     this.notes = notes;
-//     return notes;
-//   }
-//   getNotes() {
-//     return this.notes;
-//   }
-// }
-
-// const state = new State();
 //______________________________________________________________
 setInterval(() => {
   const dateObject = new Date();
@@ -240,8 +121,5 @@ setInterval(() => {
   const timeOutSec = `${time.split(" ")[0].slice(0, 5)} ${time.split(" ")[1]}`;
   state.date = date;
   state.time = timeOutSec;
-  // state.date = date;
-  // state.time = timeOutSec;
 }, 1000);
 //______________________________________________________________
-// export default state;
